76e9636401edcbbd2bc78fdbd5c73d8a7698b6ea
Soma-roj <soma.soleimanzadeh@gmail.com>
2024-02-03 18:19:49 +0300

    updated

--- apps/home.py
+++ apps/home.py
@@ -3,6 +3,9 @@ import leafmap.foliumap as leafmap
 import folium
 from streamlit_folium import st_folium
 import pandas as pd
+import numpy as np
+import base64
+import os
 
 
 def app():
@@ -10,8 +13,8 @@ def app():
         css = f.read()
     st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
 
-    m = folium.Map(location=[36.17163205508429,
-                   43.966015123674374], zoom_start=17)
+    m = folium.Map(location=[36.17217,
+                   43.96712], zoom_start=17.4)
 
     fg = folium.FeatureGroup(name="State bounds")
     fg.add_child(folium.features.GeoJson(
@@ -19,14 +22,18 @@ def app():
 
     datas = pd.read_csv(
         "assets/data/home.csv")
-
+    html = '<h5>Building 9</h5><img src="data:image/png;base64,{}">'.format
+    pic1 = base64.b64encode(open("assets/building9.jpg").read()).decode()
+    iframe1 = folium.IFrame(html(pic1),width=130+10, height=150+10)
+    popup1 = folium.Popup(iframe1,max_width=150)
+    icon1 = folium.features.CustomIcon("assets/dep-icon.png",icon_size=(40,40))
     for data in datas.itertuples():
         fg.add_child(
             folium.Marker(
                 location=[data.latitude, data.longitude],
-                popup=f"{data.index}",
-                tooltip=f"{data.name}",
-                icon=folium.Icon(color="green")
+                popup=popup1,
+                tooltip=f"{data.building_name}",
+                icon=icon1
 
             )
         )
@@ -36,13 +43,15 @@ def app():
         feature_group_to_add=fg,
         center='center',
         width=1200,
-        height=500,
+        height=600,
     )
 
-    if (out['last_object_clicked_popup']):
-        index = out['last_object_clicked_popup']
-        selectedData = datas.loc[int(index)]
-        img_1 = selectedData['image_1']
+    if (out['last_object_clicked']):
+
+        index = out['last_object_clicked']
+        selectedData = datas.index[(datas['latitude']==index['lat'])&(datas['longitude']==index['lng'])].tolist()
+        imageindex=selectedData[0]
+        img_1 = datas.loc[imageindex]['image_1']
 
         html_string = '''
         <div>
@@ -103,6 +112,12 @@ def app():
             html_string,
             unsafe_allow_html=True,
         )
+        
+        st.title(datas.loc[imageindex]['header'])
 
-        st.title(selectedData['name'])
-        st.write(selectedData['description'])
+        for i in range(9,18):
+            dept_name = datas.iloc[imageindex,i+9]
+            emptycell = datas.isnull().iloc[imageindex,i+9]
+            if not emptycell:
+                st.write(f"[{dept_name}]({datas.iloc[imageindex,i]}) - Phone: {datas.iloc[imageindex,i+18]} - Email: {datas.iloc[imageindex,i+27]}")
+
