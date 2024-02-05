c486c07d55531a51a63aceb5778d461dc150a3ce
Muhammed <muhamedsalar55@gmail.com>
2024-02-05 09:32:41 +0300

    dataset changed to json file & slider changed

--- apps/home.py
+++ apps/home.py
@@ -6,6 +6,7 @@ import pandas as pd
 import numpy as np
 import base64
 import os
+from streamlit_image_select import image_select
 
 
 def app():
@@ -20,21 +21,29 @@ def app():
     fg.add_child(folium.features.GeoJson(
         "assets/maps/home.geojson"))
 
-    datas = pd.read_csv(
-        "assets/data/home.csv")
+    dataset = pd.read_json(
+        "assets/data/home.json")
     html = '<h5>Building 9</h5><img src="data:image/png;base64,{}">'.format
-    pic1 = base64.b64encode(open("assets/building9.jpg").read()).decode()
-    iframe1 = folium.IFrame(html(pic1),width=130+10, height=150+10)
-    popup1 = folium.Popup(iframe1,max_width=150)
-    icon1 = folium.features.CustomIcon("assets/dep-icon.png",icon_size=(40,40))
-    for data in datas.itertuples():
+
+    with open("assets/building9.jpg", "rb") as imagefile:
+        convert = base64.b64encode(imagefile.read())
+    pic1 = convert.decode('utf-8')
+    iframe1 = folium.IFrame(html(pic1), width=130+10, height=150+10)
+    popup1 = folium.Popup(iframe1, max_width=150)
+    icon1 = folium.CustomIcon(
+        "assets/dep-icon.png", icon_size=(40, 40))
+    print('------------')
+    for data in dataset.datas:
+        print(data['latitude'])
+        print(data['building_name'])
+        print(popup1)
+        print(icon1)
         fg.add_child(
             folium.Marker(
-                location=[data.latitude, data.longitude],
-                popup=popup1,
-                tooltip=f"{data.building_name}",
-                icon=icon1
-
+                location=[data['latitude'], data['longitude']],
+                popup=str(popup1),
+                tooltip=f"{data['building_name']}",
+                # icon=icon1
             )
         )
 
@@ -46,78 +55,28 @@ def app():
         height=600,
     )
 
+    def get_value_by_lat_lng(objects, lat, lng):
+        for obj in objects:
+            if ('latitude' in obj and obj['latitude'] == lat) and ('longitude' in obj and obj['longitude'] == lng):
+                return obj
+        return None
+
     if (out['last_object_clicked']):
 
         index = out['last_object_clicked']
-        selectedData = datas.index[(datas['latitude']==index['lat'])&(datas['longitude']==index['lng'])].tolist()
-        imageindex=selectedData[0]
-        img_1 = datas.loc[imageindex]['image_1']
+        selectedData = get_value_by_lat_lng(
+            dataset.datas, index['lat'], index['lng'])
+        # imageindex = selectedData[0]
+        # img_1 = dataset.datas.loc[imageindex]['image']
 
-        html_string = '''
-        <div>
-            <div class="carousel">
-                <ul class="slides">
-                <input type="radio" name="radio-buttons" id="img-1" checked />
-                <li class="slide-container">
-                    <div class="slide-image">
-                    <img src="'''+img_1+'''">
-                    </div>
-                    <div class="carousel-controls">
-                    <label for="img-3" class="prev-slide">
-                        <span>&lsaquo;</span>
-                    </label>
-                    <label for="img-2" class="next-slide">
-                        <span>&rsaquo;</span>
-                    </label>
-                    </div>
-                </li>
-                <input type="radio" name="radio-buttons" id="img-2" />
-                <li class="slide-container">
-                    <div class="slide-image">
-                    <img src="'''+img_1+'''">
-                    </div>
-                    <div class="carousel-controls">
-                    <label for="img-1" class="prev-slide">
-                        <span>&lsaquo;</span>
-                    </label>
-                    <label for="img-3" class="next-slide">
-                        <span>&rsaquo;</span>
-                    </label>
-                    </div>
-                </li>
-                <input type="radio" name="radio-buttons" id="img-3" />
-                <li class="slide-container">
-                    <div class="slide-image">
-                    <img src="'''+img_1+'''">
-                    </div>
-                    <div class="carousel-controls">
-                    <label for="img-2" class="prev-slide">
-                        <span>&lsaquo;</span>
-                    </label>
-                    <label for="img-1" class="next-slide">
-                        <span>&rsaquo;</span>
-                    </label>
-                    </div>
-                </li>
-                <div class="carousel-dots">
-                    <label for="img-1" class="carousel-dot" id="img-dot-1"></label>
-                    <label for="img-2" class="carousel-dot" id="img-dot-2"></label>
-                    <label for="img-3" class="carousel-dot" id="img-dot-3"></label>
-                </div>
-                </ul>
-            </div>
-        </div>
-        '''
-        st.markdown(
-            html_string,
-            unsafe_allow_html=True,
+        img = image_select(
+            label="Select a image",
+            images=selectedData['images'],
         )
-        
-        st.title(datas.loc[imageindex]['header'])
 
-        for i in range(9,18):
-            dept_name = datas.iloc[imageindex,i+9]
-            emptycell = datas.isnull().iloc[imageindex,i+9]
-            if not emptycell:
-                st.write(f"[{dept_name}]({datas.iloc[imageindex,i]}) - Phone: {datas.iloc[imageindex,i+18]} - Email: {datas.iloc[imageindex,i+27]}")
+        if img:
+            st.image(img)
 
+        for department in selectedData['departments']:
+            st.header(department['name'], divider='rainbow')
+            st.markdown("*Streamlit* is **really** ***cool***.")
